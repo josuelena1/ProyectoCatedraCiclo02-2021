62a376e5eb89bd34b8cabc5aefdbb843796be2ef
Oscar-02 <oscacanas29@gmail.com>
2021-10-14 11:07:25 -0600

    Windows App Project added. Inserted logos into app💾

---
+++ Proyectos_ClinicaDental/Windows_ClinicaDental/Windows_ClinicaDental/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Dental Clinic Administrator")]
+[assembly: AssemblyDescription("App that manages the pacients, doctors, appoitments, etc. for all dental clinics (for Doctor Teeth). This proyect is licensed by the Creative Commons Attribution-ShareAlike 4.0 International License.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("G05T POO MDB")]
+[assembly: AssemblyProduct("Windows_ClinicaDental")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+//[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
